22ae8c911fb8eea0ad31c857fb18ce5b2c14b4dc
Deimos <jmaximilianovallejo@gmail.com>
2024-12-05 12:23:07 -0300

    cambio de nombre de archivo home a Home

--- navidad/src/components/Countdown.jsx
+++ navidad/src/components/Countdown.jsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom"; // Para manejar navegación en React Router
 import "./../styles/countdownStyles.css";
-import Snowfall from "./../components/Snowfall.jsx";
+import Snowfall from "./Snowfall";
 
 function Countdown() {
   const [timeRemaining, setTimeRemaining] = useState({});
