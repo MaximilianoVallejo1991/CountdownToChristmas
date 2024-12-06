752c906a34ea82d64a36c7aef97eb51ebe8d0f73
Deimos <jmaximilianovallejo@gmail.com>
2024-12-06 08:54:23 -0300

    agregando Home y snowfall en Home

---
+++ navidad/src/components/Home.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { fetchCountries } from "../utils/fetchCountries";
+import FlagCard from "./FlagCard";
+import "./../styles/homeStyles.css";
+import Snowfall from "./Snowfall";
+
+function Home() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetchCountries()
+      .then(setCountries)
+      .catch(console.error);
+  }, []);
+
+  const handleFlagClick = (name, timezone) => {
+    window.location.href = `/countdown?country=${name}&timezone=${timezone}`;
+  };
+
+  return (
+    
+    <div className="home-container">
+        <Snowfall />
+      <h1>Bienvenidos a la Cuenta Regresiva para Navidad</h1>
+      <p>Haz clic en la bandera para comenzar</p>
+      <div className="flags-container">
+        {countries.map((country) => (
+          <FlagCard
+            key={country.name}
+            name={country.name}
+            flag={country.flag}
+            timezone={country.timezone}
+            onClick={handleFlagClick}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
